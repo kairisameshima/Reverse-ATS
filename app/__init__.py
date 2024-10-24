24f4a9794710d9d511810a16657c21e82fe99a9e
Kairi Sameshima <kairisameshima@gmail.com>
2024-10-24 21:51:53 +0000

    Initial commit: Flask app with Posgres in a DevContainer

---
+++ app/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+# Set up the database URI
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@db:5432/mydb'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+# Import and register the blueprint
+from app.routes import main
+
+app.register_blueprint(main)
