9d5ff0e2abaace352a4702ed29ab3065aa91e840
Kairi Sameshima <kairisameshima@gmail.com>
2024-10-25 13:37:10 +0000

    Added Flask-Migrate and Black to DevContainer

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 
 # Set up the database URI
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@db:5432/mydb'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:password@db:5432/mydb"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 # Import and register the blueprint
 from app.routes import main
